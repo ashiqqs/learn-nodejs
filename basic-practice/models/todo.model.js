d621cfce8f498e8e43c728b486bc3ebe42494bc3
ashiqs <ashiqur@nybsys.com>
2021-11-08 23:21:37 +0600

    instance, static, queryHelper method

--- basic-practice/models/todo.model.js
+++ basic-practice/models/todo.model.js
@@ -1,24 +1,53 @@
-const mongoos = require('mongoose');
+const mongoos = require("mongoose");
 
 const TodoSchema = mongoos.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: String,
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    status: {
-        type: String,
-        enum: ['active', 'completed'],
-        default: 'active'
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  status: {
+    type: String,
+    enum: ["active", "completed"],
+    default: "active",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoos.model('Todo', TodoSchema);
+//Instance method
+TodoSchema.methods = {
+  getActiveTodos: () => {
+    return mongoos.model("Todo").find({
+      status: "active",
+    }).select({__v:0, date:0});
+  },
+};
+
+//static method
+
+TodoSchema.statics= {
+    getCompletedTodos: function() {
+        return this.findfind({
+            status: "completed",
+        }).select({__v:0, date:0});
+    },
+}
+
+//query helper
+
+TodoSchema.query = {
+    matchByTitle: function(title){
+        return this.find({
+            title:new RegExp(title, 'i')
+        })
+    }
+}
+
+module.exports = mongoos.model("Todo", TodoSchema);
--- basic-practice/routes/todo-route.js
+++ basic-practice/routes/todo-route.js
@@ -3,47 +3,65 @@ const router = express.Router();
 const Todo = require("../models/todo.model");
 
 router.get("/", async (req, res) => {
-    await Todo.find({status:'active'}).select({
-        __v:0,
-        date:0
-    })
-    .limit(3)
-    .exec((err, todos) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(todos);
-        }
-    })
+  try {
+    const result = await Todo.find({ status: "active" })
+      .select({
+        __v: 0,
+        date: 0,
+      })
+      .limit(3);
+    res.json(result);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-router.get("/:id", async (req, res) => {
-    await Todo.findById(req.params.id, (err, todo) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(todo);
-        }
-    })
+router.get("/active", async (req, res) => {
+  try {
+    const todo = new Todo();
+    const data = await todo.getActiveTodos().matchByTitle('Learn');
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-router.post("/", async (req, res) => {
+router.get("/completed", async (req, res) => {
+  try {
+    const data = await Todo.find().matchByTitle('earn');
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.get("/:id", (req, res) => {
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(todo);
+    }
+  });
+});
+
+router.post("/", (req, res) => {
   const todo = new Todo(req.body);
-  await todo.save((err) => {
+  todo.save((err) => {
     if (err) return res.status(500).send(err.message);
     return res.status(200).send(todo);
   });
 });
 
-router.post("/many", async (req, res) => {
-  await Todo.insertMany(req.body, (err, todos) => {
+router.post("/many", (req, res) => {
+  Todo.insertMany(req.body, (err, todos) => {
     if (err) return res.status(500).send(err.message);
     return res.status(200).send(todos);
   });
 });
 
-router.put("/:id", async (req, res) => {
-  const updatedTodo = await Todo.findByIdAndUpdate(
+router.put("/:id", (req, res) => {
+  const updatedTodo = Todo.findByIdAndUpdate(
     { _id: req.params.id },
     {
       $set: {
@@ -53,8 +71,8 @@ router.put("/:id", async (req, res) => {
       },
     },
     {
-        new: true,
-        useFindAndModify: false,
+      new: true,
+      useFindAndModify: false,
     },
     (err) => {
       if (err) return res.status(500).send(err.message);
@@ -64,11 +82,11 @@ router.put("/:id", async (req, res) => {
   console.log(updatedTodo);
 });
 
-router.delete("/:id", async (req, res) => {
-    await Todo.deleteOne({ _id: req.params.id }, (err) => {
-      if (err) return res.status(500).send(err.message);
-      return res.status(200).send("Deleted success.");
-    });
+router.delete("/:id", (req, res) => {
+  Todo.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) return res.status(500).send(err.message);
+    return res.status(200).send("Deleted success.");
+  });
 });
 
 module.exports = router;
